a8468e3df1b34b78e02f1f531b3ddb68d377a536
David Angarita <davidangaritag@gmail.com>
2019-09-07 17:26:06 -0500

    Actualizaciones view profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -7,12 +7,38 @@ import { ModelUserData } from '../../interfaces/userInterface';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  
+  hiddenSkills = true;
+  hiddenExperiences = true;
+  hiddenAccomplishments = true;
+  hiddenInterests = true;
+  hiddenEvents = true;
+  
   userData = {} as ModelUserData;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showHideSkills(){
+    this.hiddenSkills = !this.hiddenSkills;
+  }
+
+  showHideExperiences(){
+    this.hiddenExperiences = !this.hiddenExperiences;
+  }
+
+  showHideAccomplishments(){
+    this.hiddenAccomplishments = !this.hiddenAccomplishments;
+  }
+
+  showHideInterests(){
+    this.hiddenInterests = !this.hiddenInterests;
+  }
+
+  showHideEvents(){
+    this.hiddenEvents = !this.hiddenEvents;
   }
 
 }
