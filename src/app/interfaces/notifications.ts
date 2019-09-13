d6454e8bf42a3a37c30dd4869746183b6b717465
David Angarita <davidangaritag@gmail.com>
2019-09-13 16:02:54 -0500

    adecuacion notificaciones

---
+++ src/app/interfaces/notifications.ts
@@ -0,0 +1,10 @@
+
+export interface ModelNotifications {
+      event: string;
+      event_init_date: string;
+      event_end_date: string;
+      author__id: string;
+      location: string;
+      address: string;
+      event_site: string;
+}
