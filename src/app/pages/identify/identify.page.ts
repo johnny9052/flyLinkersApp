e628013a0d3de479b6c812eec2a5478810ecde1f
David Angarita <davidangaritag@gmail.com>
2019-09-24 17:00:11 -0500

    cambios mision y identify

--- src/app/pages/identify/identify.page.ts
+++ src/app/pages/identify/identify.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModelUserData, ModelUserLogIn } from '../../interfaces/userInterface';
 import { SecurityService } from '../../services/security.service';
+import { HelperService } from '../../util/HelperService';
 
 @Component({
   selector: 'app-identify',
@@ -15,7 +16,8 @@ export class IdentifyPage implements OnInit {
 
 
 
-  constructor(private securityService: SecurityService) { }
+  constructor(private securityService: SecurityService,
+              public helperService: HelperService) { }
 
   ngOnInit() {
     this.userDataLogIn.username = 'alexander9052@gmail.com';
@@ -25,6 +27,12 @@ export class IdentifyPage implements OnInit {
 
   identify() {
     this.securityService.logInUser(this.userDataLogIn);
+  }
+
+  openPage(url: string) {
+    if (url !== 'undefined' && url !== undefined && url !== null){
+      this.helperService.abrirUrlExterna(url);
+    }
   }
 
 
