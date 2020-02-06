f82a00ce4a101f58f0e893488f0a158042879c90
Johnny Alexander Salazar <alexander9052@gmail.com>
2020-02-06 10:38:08 -0500

    Version para subir playstore 06/02/2020

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+/*Import de los diferentes componentes creados*/
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { TabComponent } from './tab/tab.component';
 import { HeaderLoginComponent } from './header-login/header-login.component';
 import { FooterLoginComponent } from './footer-login/footer-login.component';
 import { FooterComponent } from './footer/footer.component';
+/*END Import de los diferentes componentes creados*/
+
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { PopcommentsComponent } from './popcomments/popcomments.component';
@@ -27,6 +31,7 @@ import { TranslateModule } from '@ngx-translate/core';
    LanguageComponent
   ],
   exports: [
+   /*Todos los componentes se que exportaran a traves del modulo*/
    HeaderComponent,
    MenuComponent,
    TabComponent,
@@ -36,6 +41,7 @@ import { TranslateModule } from '@ngx-translate/core';
    PopcommentsComponent,
    PoprecommentsComponent,
    LanguageComponent
+   /*END Todos los componentes se que exportaran a traves del modulo*/
 ],
   imports: [
     CommonModule,
--- src/app/pipes/image-sanitizer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImageSanitizerPipe } from './image-sanitizer.pipe';
-
-describe('ImageSanitizerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ImageSanitizerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
